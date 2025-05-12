b36e2da0be92b2cc1b26e9b3f3ecfd5325fbf51c
Moghadam80 <etehadian6@gmail.com>
2025-05-12 14:40:48 +0430

    debug product page

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -45,7 +45,7 @@ export default function ProductList({ products }: ProductListProps) {
         </select>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {filteredProducts.map(product => (
+        {filteredProducts?.length > 0 && filteredProducts.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
