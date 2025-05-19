f49266722e583b07b0fb7a25238698345e1d28a8
Moghadam80 <etehadian6@gmail.com>
2025-05-19 15:34:09 +0430

    add isr for product detail page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,24 @@
 import React, { Suspense } from 'react';
 import ProductDetails from './components/ProductDetails';
+import { Product } from '@/types/product';
+import { fetchAPI } from '@/utils/fetchApi';
+
+// Add revalidation time (in seconds)
+export const revalidate = 3600; // Revalidate every hour
+export const dynamicParams = true;
 
 type Props = {
   params: Promise<{
     productId: string;
   }>;
 };
+
+// Generate static paths for the most common products
+export async function generateStaticParams() {
+    const products: Product[] = await fetchAPI('/api/products', { method: 'GET' }).then((res) => res.products)
+
+    return products.map((product) => ({ productId: String(product.id) }));
+}
 
 const ProductPage = async (props: Props) => {
   const { productId } = await props.params;
